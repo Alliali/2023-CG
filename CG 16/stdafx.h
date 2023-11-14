aa449abe512c070312d2459216b16f29cf3fa08a
Alliali <leek0317@gmail.com>
2023-11-15 02:12:32 +0900

    modify

--- CG 16/stdafx.h
+++ CG 16/stdafx.h
@@ -7,6 +7,10 @@
 #include <gl/glew.h> 
 #include <gl/freeglut.h>
 #include <gl/freeglut_ext.h>
+#include <gl/glm/glm/glm.hpp>
+#include <gl/glm/glm/ext.hpp>
+#include <gl/glm/glm/gtc/matrix_transform.hpp>
+
 
 #include <vector>
 #include <random>
